62233ddd1d51ba4eac81a3d9bd85addf834a7785
GregHib <greghib@users.noreply.github.com>
2025-12-08 19:39:25 +0000

    Convert Class186

--- client/src/Class186.kt
+++ client/src/Class186.kt
@@ -1,62 +1,69 @@
 /* Class186 - Decompiled by JODE
- * Visit http://jode.sourceforge.net/
- */
+* Visit http://jode.sourceforge.net/
+*/
+abstract class Class186 {
+    abstract fun method1398(i: Int, i_3_: Int, f: Float, f_4_: Float, i_5_: Int, f_6_: Float, i_7_: Int, f_8_: Float, i_9_: Int, i_10_: Int, fs: FloatArray)
 
-public abstract class Class186 {
-    static Class45 aClass45_2490;
-    static Class74 aClass74_2491;
-    static boolean aBoolean2492 = false;
-    static int anInt2493;
-    static String[] aStringArray2494;
-    static Class351 aClass351_2495;
-    static String aString2496;
-    static int[] anIntArray2497;
+    companion object {
+        @JvmField
+        var aClass45_2490: Class45? = null
+        @JvmField
+        var aClass74_2491: Class74?
+        @JvmField
+        var aBoolean2492: Boolean = false
+        @JvmField
+        var anInt2493: Int = 0
+        @JvmField
+        var aStringArray2494: Array<String?>?
+        @JvmField
+        var aClass351_2495: Class351?
+        @JvmField
+        var aString2496: String?
+        @JvmField
+        var anIntArray2497: IntArray?
 
-    public static void method1396(int i) {
-        aClass45_2490 = null;
-        aClass74_2491 = null;
-        aStringArray2494 = null;
-        anIntArray2497 = null;
-        aString2496 = null;
-        aClass351_2495 = null;
-        if (i != 25) aClass351_2495 = null;
-    }
-
-    static final void method1397(int i) {
-        anInt2493++;
-        int i_0_ = Class328_Sub1.anInt6513;
-        int[] is = Class286_Sub7.anIntArray6290;
-        for (int i_1_ = 0; i_1_ < i_0_; i_1_++) {
-            Player player = Class294.aPlayerArray5058[is[i_1_]];
-            if (player != null && (player.anInt10264) > 0) {
-                player.anInt10264--;
-                if ((player.anInt10264) == 0) player.aString10292 = null;
-            }
+        @JvmStatic
+        fun method1396(i: Int) {
+            aClass45_2490 = null
+            aClass74_2491 = null
+            aStringArray2494 = null
+            anIntArray2497 = null
+            aString2496 = null
+            aClass351_2495 = null
+            if (i != 25) aClass351_2495 = null
         }
-        for (int i_2_ = i; i_2_ < Class150.anInt2057; i_2_++) {
-            long l = Class74.anIntArray1233[i_2_];
-            Class348_Sub22 class348_sub22 = ((Class348_Sub22) Class282.aClass356_3654.method3480(l, -6008));
-            if (class348_sub22 != null) {
-                Npc npc = (class348_sub22.aNpc_6859);
-                if ((npc.anInt10264) > 0) {
-                    npc.anInt10264--;
-                    if (npc.anInt10264 == 0) npc.aString10292 = null;
+
+        @JvmStatic
+        fun method1397(i: Int) {
+            anInt2493++
+            val i_0_ = Class328_Sub1.anInt6513
+            val `is` = Class286_Sub7.anIntArray6290
+            for (i_1_ in 0..<i_0_) {
+                val player = Class294.aPlayerArray5058[`is`[i_1_]]
+                if (player != null && (player.anInt10264) > 0) {
+                    player.anInt10264--
+                    if ((player.anInt10264) == 0) player.aString10292 = null
+                }
+            }
+            for (i_2_ in i..<Class150.anInt2057) {
+                val l = Class74.anIntArray1233[i_2_].toLong()
+                val class348_sub22 = (Class282.aClass356_3654.method3480(l, -6008) as Class348_Sub22?)
+                if (class348_sub22 != null) {
+                    val npc = (class348_sub22.aNpc_6859)
+                    if ((npc.anInt10264) > 0) {
+                        npc.anInt10264--
+                        if (npc.anInt10264 == 0) npc.aString10292 = null
+                    }
                 }
             }
         }
-    }
 
-    public Class186() {
-        /* empty */
-    }
-
-    abstract void method1398(int i, int i_3_, float f, float f_4_, int i_5_, float f_6_, int i_7_, float f_8_, int i_9_, int i_10_, float[] fs);
-
-    static {
-        aClass74_2491 = new Class74(13, -1);
-        aStringArray2494 = new String[100];
-        aClass351_2495 = new Class351(35, 5);
-        aString2496 = "";
-        anIntArray2497 = new int[25];
+        init {
+            aClass74_2491 = Class74(13, -1)
+            aStringArray2494 = arrayOfNulls<String>(100)
+            aClass351_2495 = Class351(35, 5)
+            aString2496 = ""
+            anIntArray2497 = IntArray(25)
+        }
     }
 }
--- client/src/Class186_Sub1.kt
+++ client/src/Class186_Sub1.kt
@@ -1,155 +1,193 @@
-/* Class186_Sub1 - Decompiled by JODE
- * Visit http://jode.sourceforge.net/
- */
+import jaggl.OpenGL.Companion.glTexImage2Dub
+import java.util.*
+import kotlin.math.min
 
-import jaggl.OpenGL;
+class Class186_Sub1 internal constructor(i: Int) : Class186() {
+    private val anIntArray5804 = IntArray(512)
+    override fun method1398(i: Int, i_0_: Int, f: Float, f_1_: Float, i_2_: Int, f_3_: Float, i_4_: Int, f_5_: Float, i_6_: Int, i_7_: Int, fs: FloatArray) {
+        var i_2_ = i_2_
+        anInt5810++
+        var i_8_ = (-1.0f + f * i_6_.toFloat()).toInt()
+        i_8_ = i_8_ and 0xff
+        var i_9_ = (-1.0f + i_0_.toFloat() * f_5_).toInt()
+        i_9_ = i_9_ and 0xff
+        var i_10_ = (-1.0f + i.toFloat() * f_1_).toInt()
+        i_10_ = i_10_ and 0xff
+        val f_11_ = i_7_.toFloat() * f_1_
+        var i_12_ = f_11_.toInt()
+        var i_13_ = i_12_ + 1
+        val f_14_ = f_11_ - i_12_.toFloat()
+        val f_15_ = i_4_.toFloat() - f_14_
+        val f_16_ = Class342.method2684(6, f_14_)
+        i_13_ = i_13_ and i_10_
+        i_12_ = i_12_ and i_10_
+        val i_17_ = anIntArray5804[i_12_]
+        val i_18_ = anIntArray5804[i_13_]
+        var i_19_ = 0
+        while (i_0_ > i_19_) {
+            val f_20_ = f_5_ * i_19_.toFloat()
+            var i_21_ = f_20_.toInt()
+            var i_22_ = i_21_ + 1
+            val f_23_ = -i_21_.toFloat() + f_20_
+            val f_24_ = -f_23_ + 1.0f
+            val f_25_ = Class342.method2684(i_4_ xor 0x7, f_23_)
+            i_21_ = i_21_ and i_9_
+            i_22_ = i_22_ and i_9_
+            val i_26_ = anIntArray5804[i_17_ + i_21_]
+            val i_27_ = anIntArray5804[i_17_ + i_22_]
+            val i_28_ = anIntArray5804[i_21_ + i_18_]
+            val i_29_ = anIntArray5804[i_22_ - -i_18_]
+            for (i_30_ in 0..<i_6_) {
+                val f_31_ = f * i_30_.toFloat()
+                var i_32_ = f_31_.toInt()
+                var i_33_ = 1 + i_32_
+                val f_34_ = f_31_ - i_32_.toFloat()
+                val f_35_ = 1.0f - f_34_
+                i_32_ = i_32_ and i_8_
+                i_33_ = i_33_ and i_8_
+                val f_36_ = Class342.method2684(i_4_ xor 0x7, f_34_)
+                fs[i_2_++] = f_3_ * (Class314.method2337(
+                    (Class314.method2337(
+                        (Class314.method2337((Class285.method2119(f_24_, Class139.method1166((anIntArray5804[i_26_ + i_32_]), 7), f_15_, -70, f_35_)), true, (Class285.method2119(f_24_, Class139.method1166(7, (anIntArray5804[i_33_ - -i_26_])), f_15_, -73, f_34_)), f_36_)),
+                        true,
+                        (Class314.method2337((Class285.method2119(f_23_, Class139.method1166(7, (anIntArray5804[i_32_ + i_27_])), f_15_, -75, f_35_)), true, (Class285.method2119(f_23_, Class139.method1166((anIntArray5804[i_27_ + i_33_]), 7), f_15_, -91, f_34_)), f_36_)),
+                        f_25_
+                    )),
+                    true,
+                    (Class314.method2337(
+                        (Class314.method2337((Class285.method2119(f_24_, Class139.method1166((anIntArray5804[i_28_ + i_32_]), 7), f_14_, -72, f_35_)), true, (Class285.method2119(f_24_, Class139.method1166((anIntArray5804[i_28_ + i_33_]), 7), f_14_, -75, f_34_)), f_36_)),
+                        true,
+                        (Class314.method2337((Class285.method2119(f_23_, Class139.method1166((anIntArray5804[i_29_ + i_32_]), 7), f_14_, -100, f_35_)), true, (Class285.method2119(f_23_, Class139.method1166((anIntArray5804[i_29_ + i_33_]), 7), f_14_, -72, f_34_)), f_36_)),
+                        f_25_
+                    )),
+                    f_16_
+                ))
+            }
+            i_19_++
+        }
+    }
 
-import java.util.Random;
+    init {
+        val random = Random(i.toLong())
+        for (i_41_ in 0..255) {
+            anIntArray5804[256 + i_41_] = i_41_
+            anIntArray5804[i_41_] = anIntArray5804[256 + i_41_]
+        }
+        for (i_42_ in 0..255) {
+            val i_43_ = 0xff and random.nextInt()
+            val i_44_ = anIntArray5804[i_43_]
+            anIntArray5804[256 + i_43_] = anIntArray5804[i_42_]
+            anIntArray5804[i_43_] = anIntArray5804[256 + i_43_]
+            anIntArray5804[256 + i_42_] = i_44_
+            anIntArray5804[i_42_] = anIntArray5804[256 + i_42_]
+        }
+    }
 
-public final class Class186_Sub1 extends Class186 {
-    private final int[] anIntArray5804 = new int[512];
-    static int anInt5805;
-    static int anInt5806;
-    static int anInt5807 = 0;
-    static Class341 aClass341_5808 = new Class341();
-    static int anInt5809;
-    static int anInt5810;
-    static int anInt5811;
-    static int anInt5812 = 100;
-    static Class111 aClass111_5813;
-    static int anInt5814;
+    companion object {
+        @JvmField
+        var anInt5805: Int = 0
+        @JvmField
+        var anInt5806: Int = 0
+        @JvmField
+        var anInt5807: Int = 0
+        @JvmField
+        var aClass341_5808: Class341? = Class341()
+        @JvmField
+        var anInt5809: Int = 0
+        @JvmField
+        var anInt5810: Int = 0
+        @JvmField
+        var anInt5811: Int = 0
+        @JvmField
+        var anInt5812: Int = 100
+        @JvmField
+        var aClass111_5813: Class111? = null
+        @JvmField
+        var anInt5814: Int = 0
 
-    final void method1398(int i, int i_0_, float f, float f_1_, int i_2_, float f_3_, int i_4_, float f_5_, int i_6_, int i_7_, float[] fs) {
-        anInt5810++;
-        int i_8_ = (int) (-1.0F + f * (float) i_6_);
-        i_8_ &= 0xff;
-        int i_9_ = (int) (-1.0F + (float) i_0_ * f_5_);
-        i_9_ &= 0xff;
-        int i_10_ = (int) (-1.0F + (float) i * f_1_);
-        i_10_ &= 0xff;
-        float f_11_ = (float) i_7_ * f_1_;
-        int i_12_ = (int) f_11_;
-        int i_13_ = i_12_ + 1;
-        float f_14_ = f_11_ - (float) i_12_;
-        float f_15_ = (float) i_4_ - f_14_;
-        float f_16_ = Class342.method2684(6, f_14_);
-        i_13_ &= i_10_;
-        i_12_ &= i_10_;
-        int i_17_ = anIntArray5804[i_12_];
-        int i_18_ = anIntArray5804[i_13_];
-        for (int i_19_ = 0; i_0_ > i_19_; i_19_++) {
-            float f_20_ = f_5_ * (float) i_19_;
-            int i_21_ = (int) f_20_;
-            int i_22_ = i_21_ + 1;
-            float f_23_ = (float) -i_21_ + f_20_;
-            float f_24_ = -f_23_ + 1.0F;
-            float f_25_ = Class342.method2684(i_4_ ^ 0x7, f_23_);
-            i_21_ &= i_9_;
-            i_22_ &= i_9_;
-            int i_26_ = anIntArray5804[i_17_ + i_21_];
-            int i_27_ = anIntArray5804[i_17_ + i_22_];
-            int i_28_ = anIntArray5804[i_21_ + i_18_];
-            int i_29_ = anIntArray5804[i_22_ - -i_18_];
-            for (int i_30_ = 0; i_30_ < i_6_; i_30_++) {
-                float f_31_ = f * (float) i_30_;
-                int i_32_ = (int) f_31_;
-                int i_33_ = 1 + i_32_;
-                float f_34_ = f_31_ - (float) i_32_;
-                float f_35_ = 1.0F - f_34_;
-                i_32_ &= i_8_;
-                i_33_ &= i_8_;
-                float f_36_ = Class342.method2684(i_4_ ^ 0x7, f_34_);
-                fs[i_2_++] = f_3_ * (Class314.method2337((Class314.method2337((Class314.method2337((Class285.method2119(f_24_, Class139.method1166((anIntArray5804[i_26_ + i_32_]), 7), f_15_, -70, f_35_)), true, (Class285.method2119(f_24_, Class139.method1166(7, (anIntArray5804[i_33_ - -i_26_])), f_15_, -73, f_34_)), f_36_)), true, (Class314.method2337((Class285.method2119(f_23_, Class139.method1166(7, (anIntArray5804[i_32_ + i_27_])), f_15_, -75, f_35_)), true, (Class285.method2119(f_23_, Class139.method1166((anIntArray5804[i_27_ + i_33_]), 7), f_15_, -91, f_34_)), f_36_)), f_25_)), true, (Class314.method2337((Class314.method2337((Class285.method2119(f_24_, Class139.method1166((anIntArray5804[i_28_ + i_32_]), 7), f_14_, -72, f_35_)), true, (Class285.method2119(f_24_, Class139.method1166((anIntArray5804[i_28_ + i_33_]), 7), f_14_, -75, f_34_)), f_36_)), true, (Class314.method2337((Class285.method2119(f_23_, Class139.method1166((anIntArray5804[i_29_ + i_32_]), 7), f_14_, -100, f_35_)), true, (Class285.method2119(f_23_, Class139.method1166((anIntArray5804[i_29_ + i_33_]), 7), f_14_, -72, f_34_)), f_36_)), f_25_)), f_16_));
+        @JvmStatic
+        fun method1399(i: Int, c: Char): Int {
+            anInt5805++
+            if (i != 7) method1400(-22)
+            if (c.code >= 0 && Class239_Sub6.anIntArray5902.size > c.code) return Class239_Sub6.anIntArray5902[c.code]
+            return -1
+        }
+
+        @JvmStatic
+        fun method1400(i: Int) {
+            aClass341_5808 = null
+            aClass111_5813 = null
+        }
+
+        @JvmStatic
+        fun method1401(var_ha_Sub2: ha_Sub2, i: Int, i_37_: Int, i_38_: Int, i_39_: Int, i_40_: Int): Class258_Sub3_Sub1 {
+            anInt5809++
+            if (i_37_ != 34037) aClass341_5808 = null
+            if (var_ha_Sub2.aBoolean7793 || Class192.method1436(-20, i_38_) && Class192.method1436(-119, i_40_)) return Class258_Sub3_Sub1(var_ha_Sub2, 3553, i, i_39_, i_38_, i_40_, true)
+            if (!var_ha_Sub2.aBoolean7837) return Class258_Sub3_Sub1(var_ha_Sub2, i, i_39_, i_38_, i_40_, Class33.method340(i_38_, 108.toByte()), Class33.method340(i_40_, 108.toByte()), true)
+            return Class258_Sub3_Sub1(var_ha_Sub2, 34037, i, i_39_, i_38_, i_40_, true)
+        }
+
+        @JvmStatic
+        fun method1402(i: Int) {
+            if (i == 1) {
+                Class318_Sub1_Sub2.aClass60_8732.method590(0)
+                anInt5811++
             }
         }
-    }
 
-    static final int method1399(int i, char c) {
-        anInt5805++;
-        if (i != 7) method1400(-22);
-        if (c >= 0 && Class239_Sub6.anIntArray5902.length > c) return Class239_Sub6.anIntArray5902[c];
-        return -1;
-    }
-
-    public static void method1400(int i) {
-        if (i != 1) method1401(null, 48, -14, -102, 78, 29);
-        aClass341_5808 = null;
-        aClass111_5813 = null;
-    }
-
-    static final Class258_Sub3_Sub1 method1401(ha_Sub2 var_ha_Sub2, int i, int i_37_, int i_38_, int i_39_, int i_40_) {
-        anInt5809++;
-        if (i_37_ != 34037) aClass341_5808 = null;
-        if (var_ha_Sub2.aBoolean7793 || Class192.method1436(-20, i_38_) && Class192.method1436(-119, i_40_)) return new Class258_Sub3_Sub1(var_ha_Sub2, 3553, i, i_39_, i_38_, i_40_, true);
-        if (!var_ha_Sub2.aBoolean7837) return new Class258_Sub3_Sub1(var_ha_Sub2, i, i_39_, i_38_, i_40_, Class33.method340(i_38_, (byte) 108), Class33.method340(i_40_, (byte) 108), true);
-        return new Class258_Sub3_Sub1(var_ha_Sub2, 34037, i, i_39_, i_38_, i_40_, true);
-    }
-
-    Class186_Sub1(int i) {
-        Random random = new Random(i);
-        for (int i_41_ = 0; i_41_ < 256; i_41_++)
-            anIntArray5804[i_41_] = anIntArray5804[256 + i_41_] = i_41_;
-        for (int i_42_ = 0; i_42_ < 256; i_42_++) {
-            int i_43_ = 0xff & random.nextInt();
-            int i_44_ = anIntArray5804[i_43_];
-            anIntArray5804[i_43_] = anIntArray5804[256 + i_43_] = anIntArray5804[i_42_];
-            anIntArray5804[i_42_] = anIntArray5804[256 + i_42_] = i_44_;
-        }
-    }
-
-    static final void method1402(int i) {
-        if (i == 1) {
-            Class318_Sub1_Sub2.aClass60_8732.method590(0);
-            anInt5811++;
-        }
-    }
-
-    static final void method1403(int i, int i_45_, byte[] is, boolean bool, int i_46_, int i_47_, int i_48_) {
-        anInt5806++;
-        if (i_45_ > 0 && !Class192.method1436(69, i_45_)) throw new IllegalArgumentException("");
-        if (i > 0 && !Class192.method1436(100, i)) throw new IllegalArgumentException("");
-        int i_49_ = Class183.method1382(i_46_, -6409);
-        int i_50_ = 0;
-        int i_51_ = Math.min(i_45_, i);
-        if (bool != false) anInt5807 = -127;
-        int i_52_ = i_45_ >> 1;
-        int i_53_ = i >> 1;
-        byte[] is_54_ = is;
-        byte[] is_55_ = new byte[i_52_ * (i_53_ * i_49_)];
-        for (; ; ) {
-            OpenGL.glTexImage2Dub(i_47_, i_50_, i_48_, i_45_, i, 0, i_46_, 5121, is_54_, 0);
-            if (i_51_ <= 1) break;
-            int i_56_ = i_45_ * i_49_;
-            byte[] is_57_ = is_55_;
-            for (int i_58_ = 0; i_49_ > i_58_; i_58_++) {
-                int i_59_ = i_58_;
-                int i_60_ = i_58_;
-                int i_61_ = i_56_ + i_60_;
-                for (int i_62_ = 0; i_62_ < i_53_; i_62_++) {
-                    for (int i_63_ = 0; i_63_ < i_52_; i_63_++) {
-                        int i_64_ = is_54_[i_60_];
-                        i_60_ += i_49_;
-                        i_64_ += is_54_[i_60_];
-                        i_64_ += is_54_[i_61_];
-                        i_60_ += i_49_;
-                        i_61_ += i_49_;
-                        i_64_ += is_54_[i_61_];
-                        is_55_[i_59_] = (byte) (i_64_ >> 2);
-                        i_61_ += i_49_;
-                        i_59_ += i_49_;
+        @JvmStatic
+        fun method1403(i: Int, i_45_: Int, `is`: ByteArray, bool: Boolean, i_46_: Int, i_47_: Int, i_48_: Int) {
+            var i = i
+            var i_45_ = i_45_
+            anInt5806++
+            require(!(i_45_ > 0 && !Class192.method1436(69, i_45_))) { "" }
+            require(!(i > 0 && !Class192.method1436(100, i))) { "" }
+            val i_49_ = Class183.method1382(i_46_, -6409)
+            var i_50_ = 0
+            var i_51_ = min(i_45_, i)
+            if (bool != false) anInt5807 = -127
+            var i_52_ = i_45_ shr 1
+            var i_53_ = i shr 1
+            var is_54_ = `is`
+            var is_55_ = ByteArray(i_52_ * (i_53_ * i_49_))
+            while (true) {
+                glTexImage2Dub(i_47_, i_50_, i_48_, i_45_, i, 0, i_46_, 5121, is_54_, 0)
+                if (i_51_ <= 1) break
+                val i_56_ = i_45_ * i_49_
+                val is_57_ = is_55_
+                var i_58_ = 0
+                while (i_49_ > i_58_) {
+                    var i_59_ = i_58_
+                    var i_60_ = i_58_
+                    var i_61_ = i_56_ + i_60_
+                    for (i_62_ in 0..<i_53_) {
+                        for (i_63_ in 0..<i_52_) {
+                            var i_64_ = is_54_[i_60_].toInt()
+                            i_60_ += i_49_
+                            i_64_ += is_54_[i_60_].toInt()
+                            i_64_ += is_54_[i_61_].toInt()
+                            i_60_ += i_49_
+                            i_61_ += i_49_
+                            i_64_ += is_54_[i_61_].toInt()
+                            is_55_[i_59_] = (i_64_ shr 2).toByte()
+                            i_61_ += i_49_
+                            i_59_ += i_49_
+                        }
+                        i_61_ += i_56_
+                        i_60_ += i_56_
                     }
-                    i_61_ += i_56_;
-                    i_60_ += i_56_;
+                    i_58_++
                 }
+                is_55_ = is_54_
+                is_54_ = is_57_
+                i = i_53_
+                i_45_ = i_52_
+                i_53_ = i_53_ shr 1
+                i_50_++
+                i_51_ = i_51_ shr 1
+                i_52_ = i_52_ shr 1
             }
-            is_55_ = is_54_;
-            is_54_ = is_57_;
-            i = i_53_;
-            i_45_ = i_52_;
-            i_53_ >>= 1;
-            i_50_++;
-            i_51_ >>= 1;
-            i_52_ >>= 1;
         }
     }
 }
