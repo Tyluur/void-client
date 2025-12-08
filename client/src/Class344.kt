ef5a975801495862fa4d2e139a7b9d1af0715228
GregHib <greghib@users.noreply.github.com>
2025-12-08 20:57:02 +0000

    Convert Class344

--- client/src/Class344.kt
+++ client/src/Class344.kt
@@ -1,25 +1,33 @@
 /* Class344 - Decompiled by JODE
  * Visit http://jode.sourceforge.net/
  */
+abstract class Class344 {
+    abstract fun method2689(i: Int, i_0_: Int, i_1_: Int): ByteArray?
 
-public abstract class Class344 {
-    static Class351 aClass351_4262 = new Class351(34, 15);
-    static int anInt4263 = 2;
-    static Class351 aClass351_4264 = new Class351(40, 12);
-    static int anInt4265 = 0;
-    static Class351 aClass351_4266 = new Class351(72, 16);
-    static int anInt4267;
+    abstract fun method2691(i: Byte, `is`: ByteArray)
 
-    abstract byte[] method2689(int i, int i_0_, int i_1_);
+    abstract fun method2692(i: Int): ByteArray?
 
-    public static void method2690(int i) {
-        aClass351_4266 = null;
-        aClass351_4264 = null;
-        aClass351_4262 = null;
-        int i_2_ = -29 / ((i - -54) / 45);
+    companion object {
+        @JvmField
+        var aClass351_4262: Class351? = Class351(34, 15)
+        @JvmField
+        var anInt4263: Int = 2
+        @JvmField
+        var aClass351_4264: Class351? = Class351(40, 12)
+        @JvmField
+        var anInt4265: Int = 0
+        @JvmField
+        var aClass351_4266: Class351? = Class351(72, 16)
+        @JvmField
+        var anInt4267: Int = 0
+
+        @JvmStatic
+        fun method2690(i: Int) {
+            aClass351_4266 = null
+            aClass351_4264 = null
+            aClass351_4262 = null
+            val i_2_ = -29 / ((i - -54) / 45)
+        }
     }
-
-    abstract void method2691(byte i, byte[] is);
-
-    abstract byte[] method2692(int i);
 }
