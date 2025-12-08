a3ef84e2438dceffe15aa4c46e93815645e0ff36
GregHib <greghib@users.noreply.github.com>
2025-12-08 21:49:53 +0000

    Convert Class348_Sub19

--- client/src/Class348_Sub19.kt
+++ client/src/Class348_Sub19.kt
@@ -1,11 +1,7 @@
 /* Class348_Sub19 - Decompiled by JODE
  * Visit http://jode.sourceforge.net/
  */
-
-public abstract class Class348_Sub19 extends Class348 {
-    int anInt6824;
-
-    public Class348_Sub19() {
-        /* empty */
-    }
+abstract class Class348_Sub19 : Class348() {
+    @JvmField
+    var anInt6824: Int = 0
 }
--- client/src/Class348_Sub19_Sub1.kt
+++ client/src/Class348_Sub19_Sub1.kt
@@ -1,38 +1,40 @@
 /* Class348_Sub19_Sub1 - Decompiled by JODE
  * Visit http://jode.sourceforge.net/
  */
+class Class348_Sub19_Sub1 : Class348_Sub19 {
+    @JvmField
+    var aByteArray8984: ByteArray?
+    var anInt8985: Int
+    var anInt8986: Int
+    var aBoolean8987: Boolean = false
+    var anInt8988: Int
 
-public final class Class348_Sub19_Sub1 extends Class348_Sub19 {
-    byte[] aByteArray8984;
-    int anInt8985;
-    int anInt8986;
-    boolean aBoolean8987;
-    int anInt8988;
-
-    final Class348_Sub19_Sub1 method2944(Class163 class163) {
-        this.aByteArray8984 = class163.method1272(this.aByteArray8984, 1);
-        this.anInt8988 = class163.method1270(this.anInt8988, (byte) -85);
-        if (this.anInt8986 == this.anInt8985) this.anInt8986 = this.anInt8985 = class163.method1275(-114, this.anInt8986);
-        else {
-            this.anInt8986 = class163.method1275(-83, this.anInt8986);
-            this.anInt8985 = class163.method1275(-80, this.anInt8985);
-            if (this.anInt8986 == this.anInt8985) this.anInt8986--;
+    fun method2944(class163: Class163): Class348_Sub19_Sub1 {
+        this.aByteArray8984 = class163.method1272(this.aByteArray8984, 1)
+        this.anInt8988 = class163.method1270(this.anInt8988, (-85).toByte())
+        if (this.anInt8986 == this.anInt8985) {
+            this.anInt8985 = class163.method1275(-114, this.anInt8986)
+            this.anInt8986 = this.anInt8985
+        } else {
+            this.anInt8986 = class163.method1275(-83, this.anInt8986)
+            this.anInt8985 = class163.method1275(-80, this.anInt8985)
+            if (this.anInt8986 == this.anInt8985) this.anInt8986--
         }
-        return this;
+        return this
     }
 
-    Class348_Sub19_Sub1(int i, byte[] is, int i_0_, int i_1_) {
-        this.anInt8988 = i;
-        this.aByteArray8984 = is;
-        this.anInt8986 = i_0_;
-        this.anInt8985 = i_1_;
+    internal constructor(i: Int, `is`: ByteArray?, i_0_: Int, i_1_: Int) {
+        this.anInt8988 = i
+        this.aByteArray8984 = `is`
+        this.anInt8986 = i_0_
+        this.anInt8985 = i_1_
     }
 
-    Class348_Sub19_Sub1(int i, byte[] is, int i_2_, int i_3_, boolean bool) {
-        this.anInt8988 = i;
-        this.aByteArray8984 = is;
-        this.anInt8986 = i_2_;
-        this.anInt8985 = i_3_;
-        this.aBoolean8987 = bool;
+    internal constructor(i: Int, `is`: ByteArray?, i_2_: Int, i_3_: Int, bool: Boolean) {
+        this.anInt8988 = i
+        this.aByteArray8984 = `is`
+        this.anInt8986 = i_2_
+        this.anInt8985 = i_3_
+        this.aBoolean8987 = bool
     }
 }
