d5e971cf3bead7e64147040b8965b99e50e9dd96
GregHib <greghib@users.noreply.github.com>
2025-12-08 20:59:32 +0000

    Convert Class346

--- client/src/Class346.kt
+++ client/src/Class346.kt
@@ -1,36 +1,39 @@
 /* Class346 - Decompiled by JODE
- * Visit http://jode.sourceforge.net/
- */
+* Visit http://jode.sourceforge.net/
+*/
+abstract class Class346 {
+    abstract fun method2695(i: Int)
 
-public abstract class Class346 {
-    static int anInt4274;
-    static boolean aBoolean4275;
-    static int anInt4276;
-    static int anInt4277;
+    abstract fun method2696(i: Int, i_0_: Int): Boolean
 
-    abstract void method2695(int i);
+    abstract fun method2697(i: Int): Interface6?
 
-    abstract boolean method2696(int i, int i_0_);
+    abstract fun method2698(i: Int)
 
-    public Class346() {
-        /* empty */
-    }
+    companion object {
+        @JvmField
+        var anInt4274: Int = 0
+        @JvmField
+        var aBoolean4275: Boolean = false
+        @JvmField
+        var anInt4276: Int = 0
+        @JvmField
+        var anInt4277: Int = 0
 
-    abstract Interface6 method2697(int i);
-
-    abstract void method2698(int i);
-
-    static final short[][] method2699(int i, short[][] is, float[][] fs) {
-        try {
-            if (i != 16383) return null;
-            for (int i_1_ = 0; fs.length > i_1_; i_1_++) {
-                for (int i_2_ = 0; i_2_ < is[i_1_].length; i_2_++)
-                    is[i_1_][i_2_] = (short) (int) (16383.0F * fs[i_1_][i_2_]);
+        @JvmStatic
+        fun method2699(i: Int, `is`: Array<ShortArray?>?, fs: Array<FloatArray?>?): Array<ShortArray?>? {
+            try {
+                if (i != 16383) return null
+                var i_1_ = 0
+                while (fs!!.size > i_1_) {
+                    for (i_2_ in `is`!![i_1_]!!.indices) `is`[i_1_]!![i_2_] = (16383.0f * fs[i_1_]!![i_2_]).toInt().toShort()
+                    i_1_++
+                }
+                anInt4277++
+                return `is`
+            } catch (runtimeexception: RuntimeException) {
+                throw Class348_Sub17.method2929(runtimeexception, ("efa.L(" + i + ',' + (if (`is` != null) "{...}" else "null") + ',' + (if (fs != null) "{...}" else "null") + ')'))
             }
-            anInt4277++;
-            return is;
-        } catch (RuntimeException runtimeexception) {
-            throw Class348_Sub17.method2929(runtimeexception, ("efa.L(" + i + ',' + (is != null ? "{...}" : "null") + ',' + (fs != null ? "{...}" : "null") + ')'));
         }
     }
 }
